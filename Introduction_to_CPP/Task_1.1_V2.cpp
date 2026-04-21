ffbd0787a8e0a4d80de53094231627803e63c827
AlexFedoreev <99079406+AlexFedoreev@users.noreply.github.com>
2026-04-21 21:48:48 +0300

    Add files via upload

---
+++ Introduction_to_CPP/Task_1.1_V2.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+
+#include <iostream>
+
+
+int main()   {
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+ 
+    std::cout << "Алексей\n";
+    std::cout << 26 << "\n";
+
+    return 0;
+}
