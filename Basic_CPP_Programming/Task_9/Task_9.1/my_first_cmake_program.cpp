b931d754f57e5d1c3b75c0682493e7738af86927
AlexFedoreev <99079406+AlexFedoreev@users.noreply.github.com>
2026-08-16 22:35:34 +0300

    Add files via upload

---
+++ Basic_CPP_Programming/Task_9/Task_9.1/my_first_cmake_program.cpp
@@ -0,0 +1,21 @@
+﻿// my_first_cmake_project.cpp: определяет точку входа для приложения.
+//
+
+#include <Windows.h>
+#include <locale.h>
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+	setlocale(LC_ALL, "");
+
+	std::string name{};
+	//std::cout << "Введите имя:";
+	std::cout << "Введите имя:";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << std::endl;
+}
