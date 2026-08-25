756d26930f83697494d3e26e06a32be01e2a2994
AlexFedoreev <99079406+AlexFedoreev@users.noreply.github.com>
2026-08-25 23:02:47 +0300

    Delete Basic_CPP_Programming/Task_5/Task_5.3/classFigure.cpp

--- Basic_CPP_Programming/Task_5/Task_5.3/classFigure.cpp
+++
@@ -1,9 +0,0 @@
-#include "classesFigures_h.h"
-
-
-	int Figure::numEdges = 0;
-	std::string Figure::figureName = "Фигура";
-	Figure::Figure() {};
-	void Figure::printInfoFigure() {
-		std::cout << figureName << ". Количество сторон:" << numEdges << std::endl;
-	};
