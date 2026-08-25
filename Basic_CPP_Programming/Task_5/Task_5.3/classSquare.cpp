20611cda2f31ec69d82f6ef50618d0e7d2e859ea
AlexFedoreev <99079406+AlexFedoreev@users.noreply.github.com>
2026-08-25 23:03:04 +0300

    Delete Basic_CPP_Programming/Task_5/Task_5.3/classSquare.cpp

--- Basic_CPP_Programming/Task_5/Task_5.3/classSquare.cpp
+++
@@ -1,7 +0,0 @@
-#include "classesFigures_h.h"
-
-	int Square::numEdges = 4;
-	std::string Square::figureName = "Квадрат";
-	void Square::printInfoFigure() {
-		std::cout << figureName << ". Количество сторон:" << numEdges << std::endl;
-	}
