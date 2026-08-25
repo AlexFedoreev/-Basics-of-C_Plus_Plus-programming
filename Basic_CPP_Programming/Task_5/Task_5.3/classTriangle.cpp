0623692baa03d4630639e544f08043487d0a2646
AlexFedoreev <99079406+AlexFedoreev@users.noreply.github.com>
2026-08-25 23:03:12 +0300

    Delete Basic_CPP_Programming/Task_5/Task_5.3/classTriangle.cpp

--- Basic_CPP_Programming/Task_5/Task_5.3/classTriangle.cpp
+++
@@ -1,6 +0,0 @@
-#include "classesFigures_h.h"
-	int Triangle::numEdges = 3;
-	std::string Triangle::figureName = "Треугольник";
-	void Triangle::printInfoFigure() {
-		std::cout << figureName << ". Количество сторон:" << numEdges << std::endl;
-	};
